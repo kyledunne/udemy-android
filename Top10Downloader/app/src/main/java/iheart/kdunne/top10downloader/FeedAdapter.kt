b50acff8addfb56fdb4fa3d4278bbee30418feb6
kyledunne <kyledunne@berkeley.edu>
2020-04-24 15:24:24 -0400

    Working basic menu

--- Top10Downloader/app/src/main/java/iheart/kdunne/top10downloader/FeedAdapter.kt
+++ Top10Downloader/app/src/main/java/iheart/kdunne/top10downloader/FeedAdapter.kt
@@ -1,7 +1,6 @@
 package iheart.kdunne.top10downloader
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,20 +14,16 @@ class ViewHolder(v: View) {
 }
 
 class FeedAdapter(context: Context, private val resource: Int, private val applications: List<FeedEntry>): ArrayAdapter<FeedEntry>(context, resource) {
-    private val TAG = "FeedAdapter"
     private val inflater = LayoutInflater.from(context)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        Log.d(TAG, "getView() called")
         val view: View
         val viewHolder: ViewHolder
         if (convertView == null) {
-            Log.d(TAG, "getView called with null convertView")
             view = inflater.inflate(resource, parent, false)
             viewHolder = ViewHolder(view)
             view.tag = viewHolder
         } else {
-            Log.d(TAG, "getView provided a convertView")
             view = convertView
             viewHolder = view.tag as ViewHolder
         }
@@ -43,7 +38,6 @@ class FeedAdapter(context: Context, private val resource: Int, private val appli
     }
 
     override fun getCount(): Int {
-        Log.d(TAG, "getCount() called")
         return applications.size
     }
 }
--- Top10Downloader/app/src/main/java/iheart/kdunne/top10downloader/MainActivity.kt
+++ Top10Downloader/app/src/main/java/iheart/kdunne/top10downloader/MainActivity.kt
@@ -5,7 +5,8 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
@@ -31,20 +32,48 @@ class FeedEntry {
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
-    private val downloadData by lazy { DownloadData(this, xmlListView) }
+    private var downloadData: DownloadData? = null
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            Log.d(TAG, "onCreate called")
-            downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml")
-            Log.d(TAG, "onCreate: done")
+        downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml")
+        Log.d(TAG, "onCreate: done")
+    }
+
+    private fun downloadUrl(feedUrl: String) {
+        Log.d(TAG, "downloadUrl starting AsyncTask")
+        downloadData = DownloadData(this, xmlListView)
+        downloadData?.execute(feedUrl)
+        Log.d(TAG, "downloadUrl done")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.feeds_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val feedUrl: String
+
+        when (item.itemId) {
+            R.id.mnuFree ->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml"
+            R.id.mnuPaid ->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=25/xml"
+            R.id.mnuSongs ->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/xml"
+            else -> return super.onOptionsItemSelected(item)
         }
 
-        override fun onDestroy() {
+        downloadUrl(feedUrl)
+        return true
+    }
+
+    override fun onDestroy() {
             super.onDestroy()
-            downloadData.cancel(true)
+            downloadData?.cancel(true)
         }
 
         companion object {
@@ -63,10 +92,7 @@ class MainActivity : AppCompatActivity() {
                 super.onPostExecute(result)
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
-                //Log.d(TAG, "onPostExecute: parameter is $result")
 
-//                val arrayAdapter = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parseApplications.applications)
-//                propListView.adapter = arrayAdapter
                 val feedAdapter = FeedAdapter(propContext, R.layout.list_record, parseApplications.applications)
                 propListView.adapter = feedAdapter
             }
